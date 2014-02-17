e694f30432155c700df5cb328a08b01318afd7e0
dima.rgb <dima.rgb@gmail.com>
2014-02-17 02:36:56 +0200

    add PhpStorm, Angular

--- web/yummymap.dart
+++ web/yummymap.dart
@@ -1,13 +1,29 @@
-import 'dart:html';
-//import 'package:js_wrapping/js_wrapping.dart' as js;
-import 'package:google_maps/google_maps.dart';
+library yummymap;
 
-void main() {
-  final mapOptions = new MapOptions()
-    ..zoom = 15
-    ..center = new LatLng(50.4500, 30.5233)
-    ..mapTypeId = MapTypeId.ROADMAP
-    ;  
-  final map = new GMap(querySelector("#map-canvas"), mapOptions);
-  
+import 'dart:html';
+import 'package:google_maps/google_maps.dart';
+import 'package:angular/angular.dart';
+
+@NgController(
+  selector: '[city-ctrl]'
+)
+class CityController extends NgController {
+
+  CityController() {
+    final mapOptions = new MapOptions()
+      ..zoom = 15
+      ..center = new LatLng(50.4500, 30.5233)
+      ..mapTypeId = MapTypeId.ROADMAP
+    ;
+    final map = new GMap(querySelector('#map-canvas'), mapOptions);
+  }
+
 }
+
+class YummymapModule extends Module {
+	YummymapModule() {
+		type(CityController);
+	}
+}
+
+main() => ngBootstrap(module: new YummymapModule());
